35152128f234f48b2ecdd03fa84bc5c6c35fac6c
SupunArunoda <supunarunoda@gmail.com>
2017-07-05 11:05:23 +0530

    sequential matrix multiplication is completed

---
+++ sequential_matrix_multi.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#define RANDLIMIT	7	/* Magnitude limit of generated randno.*/
+#define N		10  	/* Matrix Size */
+#define NUMLIMIT 120.0
+
+float matrix_A[N][N];//input matrix 1
+float matrix_B[N][N];//input matrix 2
+float matrix_C[N][N];//output matrix
+
+int main(int argc, char *argv[])
+{
+	struct timeval start, stop;
+	int i, j, k;
+
+
+	/*Geenerate matrixes */
+	for (i = 0; i < N; i++){
+	for (j = 0; j < N; j++) {
+		matrix_A[i][j] = 1 + (int)(NUMLIMIT*rand() / (RAND_MAX + 1.0));
+		matrix_B[i][j] = (double)(rand() % RANDLIMIT);
+
+	}
+	}
+
+
+	/*Matrix multiplication*/
+	gettimeofday(&start, 0);//start the time
+
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			matrix_C[i][j] = 0.0;
+			for (k = 0; k < N; k++)
+				matrix_C[i][j] = matrix_C[i][j] + matrix_A[i][k] * matrix_B[k][j];
+		} 
+	}
+
+	gettimeofday(&stop, 0);//stop the time
+
+	//Consumed time
+	fprintf(stdout, "Time = %.6f\n\n",
+		(stop.tv_sec + stop.tv_usec*1e-6) - (start.tv_sec + start.tv_usec*1e-6));
+	return(0);
+}
+
